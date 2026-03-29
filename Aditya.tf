3f77fc8b925d6927fc47d2cc6836f90ed2b0f597
Aditya <rathoreadi24@gmail.com>
2026-03-29 12:50:14 +0530

    Aditya.tf new update

---
+++ Aditya.tf
@@ -0,0 +1,12 @@
+variable "resource_group_name" {
+  description = "Name of the resource group"
+  type        = string
+  default     = "demo-rg"
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "${var.prefix}-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.gnet.name
+  address_prefixes     = ["10.0.1.0/28"]
+}
